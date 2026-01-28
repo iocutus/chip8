da7a7426b4a584b8ff266e1db5ec2baa5f818457
iocutus <iocutus.gamedev@gmail.com>
2026-01-28 01:35:39 +0100

    Add main method stumps

--- chip.go
+++ chip.go
@@ -11,3 +11,20 @@ type ChipContext struct {
 	SoundReg    uint16
 	FrameBuffer [ScreenWidth * ScreenHeight]bool
 }
+
+func (c *ChipContext) fetch() {
+	// fetch instruction pc is pointing at from memory
+	// the first two successive bytes
+	// combine them into one 16 bit instruction
+
+	// increment pc by 2
+	c.PC += 2
+}
+
+func decode() {
+
+}
+
+func execute() {
+
+}
